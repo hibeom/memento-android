6af38850b271e7ea90e3a45f320e7e7c8da78737
hibeom <hibeom@DESKTOP-NEQ25AE.localdomain>
2021-08-11 10:40:26 +0900

    Priority slider gradient color

--- app/src/main/java/com/pinkcloud/memento/common/PrioritySlider.kt
+++ app/src/main/java/com/pinkcloud/memento/common/PrioritySlider.kt
@@ -7,15 +7,22 @@ import android.graphics.Color
 import android.util.AttributeSet
 import com.google.android.material.slider.Slider
 import com.pinkcloud.memento.R
+import timber.log.Timber
 
 class PrioritySlider @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : Slider(context, attrs, defStyleAttr) {
 
-
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        trackActiveTintList = if (value < 50) {
+            var red = 225/50f*(value+1) + 30
+            var green = 92/50f*(value+1) + 150
+            ColorStateList.valueOf(Color.rgb(red.toInt(), green.toInt(), 0))
+        } else {
+            var green = 242 - 242/50f*(value-50)
+            ColorStateList.valueOf(Color.rgb(255, green.toInt(), 0))
+        }
 
     }
 }
--- app/src/main/java/com/pinkcloud/memento/database/MemoDatabaseDao.kt
+++ app/src/main/java/com/pinkcloud/memento/database/MemoDatabaseDao.kt
@@ -16,10 +16,10 @@ interface MemoDatabaseDao {
     suspend fun delete(memo: Memo)
 
     // Query dao with returning LiveData is suspend fun as far as I know.
-    @Query("SELECT * FROM memo_table WHERE is_completed = 0")
+    @Query("SELECT * FROM memo_table WHERE is_completed = 0 ORDER BY priority DESC")
     fun getOngoingMemos(): LiveData<List<Memo>>
 
-    @Query("SELECT * FROM memo_table WHERE is_completed = 1")
+    @Query("SELECT * FROM memo_table WHERE is_completed = 1 ORDER BY priority DESC")
     fun getCompletedMemos(): LiveData<List<Memo>>
     
     @Query("DELETE FROM memo_table WHERE is_completed = 1 AND :currentTimeInMillis - completed_time >= 7*24*60*60*1000")
